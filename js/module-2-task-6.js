05225b746e52498b51376cb98fccab7a4cdaff18
Dmytro Petrenko <blinker182ca@gmail.com>
2020-08-03 14:21:28 +0300

    done 6th task

--- js/module-2-task-6.js
+++ js/module-2-task-6.js
@@ -1,2 +1,37 @@
 console.log('--- Task 6 ---');
 
+let result = 0;
+
+function mapArray(array) {
+	let numbers = new Array(array.length);
+	numbers.push(array);
+	numbers.splice(0, 3);
+
+	// console.log(numbers);
+
+	for (let i = 0; i < array.length; i += 1) {
+		// Write code under this line
+		// console.log(array[i] * 10);
+		numbers[i] = array[i] * 10;
+		// console.log(array[i]);
+		// numbers.push(array);
+	}
+
+	return numbers;
+	// console.log(numbers);
+}
+
+console.log(mapArray([ -2, 0, 2 ]));
+// [-20, 0, 20]
+
+//console.log(mapArray([-2.5, 0, 2.5]));
+// [-25, 0, 25]
+
+// const arr = [ 1, 2, 3, 4, 5 ];
+// console.log(arr);
+
+// for (let i = 0; i < arr.length; i += 1) {
+// 	const res = arr[i];
+
+// 	console.log(res);
+// }
